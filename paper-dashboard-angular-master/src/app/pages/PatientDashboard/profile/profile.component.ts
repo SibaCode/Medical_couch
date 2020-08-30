87c4acf46468745dcee89d1ad8841a64c8d5467e
Sibusiso <cj.sibusiso@gmail.com>
2020-08-30 12:19:58 +0200

    small things

--- paper-dashboard-angular-master/src/app/pages/PatientDashboard/profile/profile.component.ts
+++ paper-dashboard-angular-master/src/app/pages/PatientDashboard/profile/profile.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { User } from '../../../models/user';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { PatientService } from '../../../services/patient.service';
-
+import Swal from 'sweetalert2'
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
@@ -30,6 +30,22 @@ update() {
 
   this.patServ.update(this.userForm.value).subscribe( (res) => {
     localStorage.setItem('user',JSON.stringify(this.userForm.value))
+    const Toast = Swal.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      onOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+      }
+    })
+    
+    Toast.fire({
+      icon: 'success',
+      title: 'Updated successfully'
+    })
   }, (err) => {
     console.log(err)
   })
