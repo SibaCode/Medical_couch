fc97b05c8cdd1a12d238a16ea022b2eb9de4e5b3
SibaCode <mvubusiba@gmail.com>
2020-08-30 05:55:55 +0200

    patient

---
+++ paper-dashboard-angular-master/src/app/pages/PatientDashboard/profile/profile.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../../models/user';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { PatientService } from '../../../services/patient.service';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+
+  user: User
+  userForm: FormGroup
+
+  constructor(private fb: FormBuilder , private patServ: PatientService) {
+    this.user = JSON.parse(localStorage.getItem('user')) ;;
+    this.user= "sbdua" ;
+    this.userForm = fb.group(this.user)
+ }
+
+ngOnInit() {
+
+
+  console.log(this.user)
+}
+
+
+update() {
+
+  this.patServ.update(this.userForm.value).subscribe( (res) => {
+    localStorage.setItem('user',JSON.stringify(this.userForm.value))
+  }, (err) => {
+    console.log(err)
+  })
+  console.log(this.userForm.value)
+}
+
+
+}
